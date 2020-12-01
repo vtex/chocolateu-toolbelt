0d35801ca0c2cb396929eb9286ca2a1b56b968e8
Thiago Veras <thiago.veras.machado@gmail.com>
2020-12-01 18:21:37 -0300

    Base files

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://vtex-toolbelt-test.s3.us-east-2.amazonaws.com/vtex-x64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url      = $url
+
+  softwareName  = 'vtex*'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+New-Item -ItemType SymbolicLink -Path 'C:\Program Files\vtex\client\node_modules\vtex' -Target 'C:\Program Files\vtex\client'    
