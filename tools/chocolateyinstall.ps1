b098be5dd2a69d5605699a1f91e4ae0edd81e22f
Thiago Veras <thiago.veras.machado@gmail.com>
2020-12-02 20:06:48 -0300

    Refactor based on Heroku configs file

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
+$packageName = 'vtex-cli'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://vtex-toolbelt-test.s3.us-east-2.amazonaws.com/vtex-x64.exe'
+
+$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing x64 bit version"; Get-Item "$toolsDir\*x64.exe"
+} else { Write-Host "Installing x32 bit version"; Get-Item "$toolsDir\*x86.exe" }
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url      = $url
-
-  softwareName  = 'vtex*'
-
-  silentArgs    = "/S"
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = $installerFile
+  softwareName  = 'VTEX CLI'
+  silentArgs   = '/S'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
+Remove-Item ($toolsDir + '\*.' + $packageArgs.fileType)
 New-Item -ItemType SymbolicLink -Path 'C:\Program Files\vtex\client\node_modules\vtex' -Target 'C:\Program Files\vtex\client'    
